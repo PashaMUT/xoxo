b7424abc5c3c074b35830bb43ad55f603f31ae0f
Paxa <p.mutovkin@beloil.by>
2025-09-26 16:58:14 +0300

    fix: remove unused Intro import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Intro from "./components/Intro";
+
 import Services from "./sections/Services";
 import Footer from "./components/Footer";
 
 const App: React.FC = () => {
     return (
         <div>
-            {/*<Intro />*/}
             <Header />
             <Hero />
             <Services/>
